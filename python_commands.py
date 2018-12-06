453c1eea05a9afbd17a07ef69a87e56584805efb
Oktawiusz Wieczorek <oktawiusz.wieczorek@cbc.de>
2018-12-06 16:23:06 +0100

    Add files via upload

---
+++ python_commands.py
@@ -0,0 +1,5 @@
+#############################DEBUG##########################
+cwd = os.getcwd()  # Get the current working directory (cwd)
+files = os.listdir(cwd)  # Get all the files in that directory
+print("Files in '%s': %s" % (cwd, files))
+#############################DEBUG##########################
